79e5f6c0c56dc4f47f7da7525ca50d477417424a
김민조 <mjkim@tricycle.co.kr>
2023-07-28 10:08:39 +0900

    prettier tab수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        es2021: true,
-        node: true
-    },
-    ignorePatterns: ["node_modules/*", "dist/*", "!.prettierrc.js"],
-    extends: ["plugin:nuxt/recommended", "plugin:vue/vue3-recommended", "plugin:prettier/recommended"],
-    plugins: ["vue", "prettier"],
-    rules: {}
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  ignorePatterns: ["node_modules/*", "dist/*"],
+  extends: ["plugin:nuxt/recommended", "plugin:vue/vue3-recommended", "plugin:prettier/recommended"],
+  plugins: ["vue", "prettier"],
+  rules: {}
 }
